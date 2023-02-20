cf0748bf8be5d54127fa7464a7e8d5f8cfe57945
uibeanuka <uibeanuka@gmail.com>
2023-02-20 01:09:18 +0100

    final changes

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -15,7 +15,7 @@ async function main() {
 }
 
 // We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
+// and properly handle errors errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
